bc9cd885591d5abf930396945f0f0ef736a6cc22
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-22 15:34:59 -0300

    feat: heap adicionado

---
+++ Fundamentos da programação/Heap/index.ts
@@ -0,0 +1,12 @@
+var n: number = 10; // Vai alocar um espaço na memória para guardar o valor "10"
+const myName: string = "Carlos"; // Vai alocar um espaço na memória para guardar a string "Carlos"
+
+// ==> Alocando e desalocando memória no JS
+addEventListener // Adiciona na memória
+removeEventListener // Remove da memória
+
+setTimeout // Adiciona na memória
+clearTimeout // Remove da memória
+
+setInterval // Adiciona na memória
+clearInterval // Remove da memória
