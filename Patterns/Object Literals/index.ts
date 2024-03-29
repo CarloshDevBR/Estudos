c305a65ecb33d1d456d9b56b0628206de04498f2
CarlosHDevBR <carlos.sortudox1@gmail.com>
2024-03-29 12:39:29 -0300

    feat: pattern object literals criado

--- Patterns/Object Literals/index.ts
+++ Patterns/Object Literals/index.ts
@@ -1,16 +1,16 @@
-type KeysProps = 'arrowUp' | 'arrowRight' | 'arrowDown' | 'arrowLeft'
+const keyPressed = 'arrowLeft'
 
-const movePlayer = {
+const acceptedMoves = {
     arrowUp: () => console.log('moveu pra cima'),
-    arrowRight: () => console.log('moveu pra direita'),
     arrowDown: () => console.log('moveu pra baixo'),
-    arrowLeft: () => console.log('moveu pra esquerda')
+    arrowLeft: () => console.log('moveu pra esquerda'),
+    arrowRight: () => console.log('moveu pra direita')
 }
 
-function handleKeyPress(key: KeysProps) {
-    const move = movePlayer[key]
+const moveFunction = (keyPressed: string) => {
+    const action = acceptedMoves[keyPressed]
 
-    move()
+    action()
 }
 
-handleKeyPress('arrowUp')
+moveFunction(keyPressed)
